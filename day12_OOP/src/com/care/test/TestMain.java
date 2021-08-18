10d1d9f41296d37338187ba587e12fded489a840
jungyooseok <wjddbtjr8888@naver.com>
2021-08-18 09:50:14 +0900

    사과

---
+++ day12_OOP/src/com/care/test/TestMain.java
@@ -0,0 +1,21 @@
+package com.care.test;
+
+
+import java.util.Date;
+
+import com.care.zzz.*;
+import com.care.zzz.TestClass;
+public class TestMain {
+	public static void main(String[] args) {
+		
+		
+		Test t = new Test();
+		t.test();
+		TestClass tc = new TestClass();
+		tc.testClass();
+		TestClass02 tc02 = new TestClass02();
+		tc02.testClass02();
+		
+		a.a.a.TestClass tt = new a.a.a.TestClass();
+	}
+}
